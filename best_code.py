4bf0cbb000fe09bfac531ff149c5e48df3408ed2
eoxe316 <109499021+eoxe316@users.noreply.github.com>
2024-05-08 13:45:15 +1200

    Minor code cleanup

--- best_code.py
+++ best_code.py
@@ -6,6 +6,54 @@ import numpy as np
 import csv
 import math
 
+# Function definitions
+
+def find_weld_gap(height_index, line_image, main_image):
+    weld_line = line_image[height_index, :]
+    weld_positions = []
+    for index in range(len(weld_line) - 1):
+        if list(weld_line[index]) == [0,255,0]:
+            weld_positions.append(index)
+    
+    cv2.line(main_image, (0, 70), (len(weld_line) - 1, 70), (255,0,0),5)
+    if len(weld_positions) != 0:
+        cv2.line(main_image, (min(weld_positions), 70), (max(weld_positions), 70), (0,0,255),10)
+    
+    return main_image, weld_positions
+
+def save_image(file_name, image, interim_no):
+    # Saves image with filename format requested
+    # If final image is desired, set interim_no to 0
+    name_end = file.find(".")
+    image_title = file_name[0:name_end]
+    if interim_no == 0:
+        output_name = f'{image_title.capitalize()}_A_WeldGapPosition.jpg'
+    else:
+        output_name = f'{image_title.capitalize()}_B_InterimResult{interim_no}.JPG '
+    # Define the output file path
+    fout = os.path.join(outPar, output_name)
+    # Save the grayscale image with detected edges
+    cv2.imwrite(fout, image)
+
+def check_validity(weld_indices, last_gap_position):
+    try:
+        # check the distance of the weld indices
+        width = max(weld_indices)- min(weld_indices)
+        location = math.floor(width/2) + min(weld_indices)
+        if (width <11 and len(weld_indices) >=3 and ((last_gap_position == 0) or abs(last_gap_position-location)<22)):
+            # Check width is within tolerance and that hasnt moved more that 1mm from the last image
+            # Also check that there is at least 3 measurements for the weld line
+            last_gap_position = location
+            return location, 1
+        else:
+            return -1, 0
+    except ValueError:
+        # No weld indices exist
+        return -1, 0
+
+
+# Main code start
+
 # Get the current working directory
 path = os.getcwd()
 
@@ -17,23 +65,16 @@ os.makedirs(outPar, exist_ok=True)
 
 # List all files in the input directory
 files = os.listdir(inputPar)
-# files = [files[0]]
-last_gap_position = 0
 
+#Initialise weld positions array and last logged weld position
+last_gap_position = 0
 weld_positions = []
+
 
 for file in files:
     fitem = os.path.join(inputPar, file)
     img = cv2.imread(fitem)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Kernal used to filter image
-    # kernel = np.array([
-    #     [0, -1, 0],
-    #     [-1, 5, -1],
-    #     [0, -1, 0]
-    # ])
 
     kernel = np.array([
         [-1, 0, 1],
@@ -45,38 +86,13 @@ for file in files:
     kernel_image = cv2.filter2D(blurred, -1, kernel)
     image_filtered = kernel_image
 
-    # #Log filtering of image
-    # c = 255 / np.log(1 + np.max(kernel_image)) 
-    # log_image = c * (np.log(kernel_image + 1))
-    # log_image[np.isneginf(log_image)] = 255
-    # log_image = np.uint8(log_image)
-
-    # image_filtered = log_image
-    # image_filtered = cv2.equalizeHist(log_image)
-    # image_filtered = cv2.Sobel(kernel_image, cv2.CV_64F,0,1,ksize=3) 
-    # image_filtered = cv2.Canny(kernel_image, 50, 150, apertureSize=3)
-    # exp_image = np.power(gray / 255.0, 5) * 255.0
-
     upper_limit = 255
-    lower_limit = 255
+    lower_limit = 80
 
     ret, thresh1 = cv2.threshold(image_filtered, lower_limit, upper_limit, cv2.THRESH_BINARY)
-    ret, thresh2 = cv2.threshold(image_filtered, lower_limit, upper_limit, cv2.THRESH_BINARY_INV)
-    ret, thresh3 = cv2.threshold(image_filtered, lower_limit, upper_limit, cv2.THRESH_TRUNC)
-    ret, thresh4 = cv2.threshold(image_filtered, lower_limit, upper_limit, cv2.THRESH_TOZERO)
-    ret, thresh5 = cv2.threshold(image_filtered, lower_limit, upper_limit, cv2.THRESH_TOZERO_INV)
-
-    thresh = np.concatenate((blurred, image_filtered, thresh1, thresh2, thresh3, thresh4, thresh5), axis=1)
-    thresh = cv2.resize(thresh, (1400, 540), interpolation = cv2.INTER_AREA)
-
-    # cv2.imshow('Thresholded images', thresh)
-    # cv2.waitKey(0)
-
     edges = thresh1
 
     # This returns an array of r and theta values
-    lines_list =[]
-
     lines = cv2.HoughLinesP(
             edges, # Input edge image
             1, # Distance resolution in pixels
@@ -88,6 +104,7 @@ for file in files:
  
     # Iterate over points
     try:
+        lines_list =[]
         for points in lines:
             # Extracted points nested in the list
             x1,y1,x2,y2=points[0]
@@ -100,49 +117,6 @@ for file in files:
         # No lines exist
         continue
 
-    def find_weld_gap(height_index, line_image, main_image):
-        weld_line = line_image[height_index, :]
-        weld_positions = []
-        for index in range(len(weld_line) - 1):
-            if list(weld_line[index]) == [0,255,0]:
-                weld_positions.append(index)
-        
-        cv2.line(main_image, (0, 70), (len(weld_line) - 1, 70), (255,0,0),5)
-        if len(weld_positions) != 0:
-            cv2.line(main_image, (min(weld_positions), 70), (max(weld_positions), 70), (0,0,255),10)
-        
-        return main_image, weld_positions
-    
-    def save_image(file_name, image, interim_no):
-        # Saves image with filename format requested
-        # If final image is desired, set interim_no to 0
-        name_end = file.find(".")
-        image_title = file_name[0:name_end]
-        if interim_no == 0:
-            output_name = f'{image_title.capitalize()}_A_WeldGapPosition.jpg'
-        else:
-            output_name = f'{image_title.capitalize()}_B_InterimResult{interim_no}.JPG '
-        # Define the output file path
-        fout = os.path.join(outPar, output_name)
-        # Save the grayscale image with detected edges
-        cv2.imwrite(fout, image)
-
-    def check_validity(weld_indices, last_gap_position):
-        try:
-            # check the distance of the weld indices
-            width = max(weld_indices)- min(weld_indices)
-            location = math.floor(width/2) + min(weld_indices)
-            if (width <11 and len(weld_indices) >=3 and ((last_gap_position == 0) or abs(last_gap_position-location)<22)):
-                # Check width is within tolerance and that hasnt moved more that 1mm from the last image
-                # Also check that there is at least 3 measurements for the weld line
-                last_gap_position = location
-                return location, 1
-            else:
-                return -1, 0
-        except ValueError:
-            # No weld indices exist
-            return -1, 0
-
     weld_image, weld_indices = find_weld_gap(70, img, img)
     weld_location, weld_valid = check_validity(weld_indices, last_gap_position)
     if weld_location != -1:
@@ -153,6 +127,8 @@ for file in files:
     weld_positions.append([file, weld_location, weld_valid])
 
     print_image = cv2.resize(img, (1400, 540), interpolation = cv2.INTER_AREA)
+
+    # Shows image if uncommented
     cv2.imshow('test', print_image)
     cv2.waitKey(0)
     
